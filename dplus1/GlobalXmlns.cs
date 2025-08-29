8bd058ba2da03e1f5b5abab658c68c9b4d0368eb
Eduardo CFN <eduardo@verion.ind.br>
2025-08-29 20:58:13 -0300

    chore [dplus1] -Created a new .NET MAUI App with name dplus1.

---
+++ dplus1/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "dplus1")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "dplus1.Pages")]
