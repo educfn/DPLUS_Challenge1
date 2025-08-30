6f24df91aaf28ae8afa2a6d45c7673e12d53e265
Eduardo CFN <eduardo@verion.ind.br>
2025-08-30 09:46:38 -0300

    fix [Client.cs] - Fixing some warnings about uninitiated properties.

--- dplus1/Model/Client.cs
+++ dplus1/Model/Client.cs
@@ -2,8 +2,8 @@
 
 public class Client
 {
-    public string Name { get; set; }
-    public string Lastname { get; set; }
-    public int Age { get; set; }
-    public string Address { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public int Age { get; set; } = 0;
+    public string Address { get; set; } = string.Empty;
 }
