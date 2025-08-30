142161b97958c6227abeea46a4dfd54922a6c2cd
Eduardo CFN <eduardo@verion.ind.br>
2025-08-30 09:38:31 -0300

    Chore [Client.cs] - Created the model class 'Client'.

---
+++ dplus1/Model/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace dplus1.Model;
+
+public class Client
+{
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+}
