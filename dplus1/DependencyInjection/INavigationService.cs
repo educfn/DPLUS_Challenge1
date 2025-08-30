d491c2dd9afdc945fd4d7419f82c11a328d61720
Eduardo CFN <eduardo@verion.ind.br>
2025-08-30 19:30:44 -0300

    chore [Navigating Service]: - Implementing the Navigation. --Created the interface and the class implementing the interface.

---
+++ dplus1/DependencyInjection/INavigationService.cs
@@ -0,0 +1,7 @@
+﻿namespace dplus1.DependencyInjection;
+
+public interface INavigationService
+{
+    Task PushAsync<TPage>() where TPage : Page;
+    Task PopAsync();
+}
---
+++ dplus1/DependencyInjection/NavigationService.cs
@@ -0,0 +1,26 @@
+﻿
+namespace dplus1.DependencyInjection;
+
+public class NavigationService : INavigationService
+{
+    private readonly IServiceProvider _services;
+
+    public NavigationService(IServiceProvider services)
+    {
+        _services = services;
+    }
+
+    private INavigation Navigation
+        => Application.Current?.MainPage?.Navigation
+            ?? throw new InvalidOperationException("Navigation not available");
+
+    public Task PushAsync<TPage>() where TPage : Page
+    {
+        return Navigation.PushAsync(_services.GetRequiredService<TPage>());
+    }
+
+    public Task PopAsync()
+    {
+        return Navigation.PopAsync();
+    }
+}
