5d7eeea539bb27ebb3a81a0adcfb77bc14a15dc9
Eduardo CFN <eduardo@verion.ind.br>
2025-08-30 10:30:55 -0300

    feat [NavigationPage] - Implemented navigation through NavigationPage class.

--- dplus1/App.xaml.cs
+++ dplus1/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace dplus1;
+﻿using dplus1.Views;
+
+namespace dplus1;
 
 public partial class App : Application
 {
@@ -9,6 +11,6 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new AppShell());
+		return new Window(new NavigationPage(new ClientsListView()));
 	}
 }
