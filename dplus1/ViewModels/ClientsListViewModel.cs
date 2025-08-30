3a8b99f61461890d44ec9d1f32711991c6a40ca9
Eduardo CFN <eduardo@verion.ind.br>
2025-08-30 13:06:11 -0300

    feat [ClientList - Navigation] - Added the 'ClientList' view model to its BindingContext; removed unused controls; Changed Button control to ClientView navigation. - Installed the Nuget "CommunityToolkit.Mvvm" from Microsoft to use in the navigation, IComamnd and other stuff. - ViewModel: Added the ICommand property to be passed to the View. - Implemented AsyncRelayCommand with the options to ignore concurrent calls. - NavigateToClient: Method will be filled later after the ClientView   creation.

---
+++ dplus1/ViewModels/ClientsListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace dplus1.ViewModels;
+
+public class ClientsListViewModel
+{
+    public ClientsListViewModel()
+    {
+        CommandNavigateToClientView = new AsyncRelayCommand(NavigateToClient, AsyncRelayCommandOptions.None);
+    }
+
+    public ICommand CommandNavigateToClientView { get; }
+
+    private Task NavigateToClient()
+    {
+        return Task.CompletedTask;
+    }
+}
