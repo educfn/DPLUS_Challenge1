2859958299dfdf01289101cf6bc690b555e51666
Eduardo CFN <eduardo@verion.ind.br>
2025-08-31 14:30:57 -0300

    Feat [ClientListView]: - Added the list of Clients and the 'Add', 'Modify' and 'Delete' buttons to the view. - Added the list of clients to the view model.
    obs: Besides the CollectionView, the buttons doesn't have real function(for now).

--- dplus1/ViewModels/ClientsListViewModel.cs
+++ dplus1/ViewModels/ClientsListViewModel.cs
@@ -1,25 +1,29 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 using dplus1.DependencyInjection;
+using dplus1.Model;
 using dplus1.Views;
 
 namespace dplus1.ViewModels;
 
 public class ClientsListViewModel
 {
-    private readonly INavigationService _navigationService;
+    private readonly INavigationService navigationService;
 
     public ClientsListViewModel(INavigationService navigationService)
     {
-        _navigationService = navigationService;
+        this.navigationService = navigationService;
+        AllClients = new ObservableCollection<Client>();
 
         CommandNavigateToClientView = new AsyncRelayCommand(NavigateToClient, AsyncRelayCommandOptions.None);
     }
 
     public ICommand CommandNavigateToClientView { get; }
+    public ObservableCollection<Client> AllClients { get; }
 
     private async Task NavigateToClient()
     {
-        await _navigationService.PushAsync<ClientView>();
+        await this.navigationService.PushAsync<ClientView>();
     }
 }
