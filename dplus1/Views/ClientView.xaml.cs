fedbdc4955a2acb90e630b3203368c4502741b80
Eduardo C F Neto <eduardo@verion.ind.br>
2025-09-01 11:04:31 -0300

    feat [ClientView - PopUp] - Added a PopUp in the code behind to warn the user about the risk of deleting a client.

--- dplus1/Views/ClientView.xaml.cs
+++ dplus1/Views/ClientView.xaml.cs
@@ -4,9 +4,20 @@ namespace dplus1.Views;
 
 public partial class ClientView : ContentPage
 {
+	private ClientViewModel vm;
+
 	public ClientView(ClientViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
+		this.vm = vm;
 	}
+
+	private async void RemoveClientButtonClicked(object sender, EventArgs e)
+    {
+        bool answer = await DisplayAlert("Attention", "Are you sure you want to delete?", "Yes", "No");
+
+		if (answer)
+			vm.CommandRemove.Execute(null);
+    }
 }
